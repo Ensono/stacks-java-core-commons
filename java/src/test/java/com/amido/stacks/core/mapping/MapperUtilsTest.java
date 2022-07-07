7d6e6fdfbbf2edea6ac82464417fbb8d932fe49a
Steve Davis <steve.davis@amido.com>
2022-07-07 11:17:01 +0100

    Initial commit for MapStruct dependencies

---
+++ java/src/test/java/com/amido/stacks/core/mapping/MapperUtilsTest.java
@@ -0,0 +1,25 @@
+package com.amido.stacks.core.mapping;
+
+import static com.amido.stacks.core.mapping.MapperUtils.map;
+import static java.util.UUID.randomUUID;
+
+import java.util.UUID;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MapperUtilsTest {
+
+  @Test
+  public void shouldConvertBetweenUuidAndString() {
+
+    UUID uuid = randomUUID();
+    Assertions.assertEquals(map(uuid), uuid.toString());
+  }
+
+  @Test
+  public void shouldConvertBetweenStringAndUuid() {
+
+    UUID uuid = randomUUID();
+    Assertions.assertEquals(map(uuid.toString()), uuid);
+  }
+}
