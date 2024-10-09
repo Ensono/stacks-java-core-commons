590dcbd5d843559af6fcbb7a3561a21f5dec60cc
Jack Blower <Jack@elvenspellmaker.co.uk>
2024-10-09 12:57:34 +0100

    fix: fixes tests as they have wrong order

--- java/src/main/java/com/ensono/stacks/core/repository/StacksPersistence.java
+++ java/src/main/java/com/ensono/stacks/core/repository/StacksPersistence.java
@@ -1,6 +1,8 @@
 package com.ensono.stacks.core.repository;
 
+import java.lang.SuppressWarnings;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@SuppressWarnings("java:S2326") // "T is not used in the interface". This Interface is used by other modules in other stacks repos.
 public interface StacksPersistence<T> {}
