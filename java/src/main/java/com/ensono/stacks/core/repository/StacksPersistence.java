146188afbc1cc7b22560ae70db2380de4f4c8399
Jack Blower <Jack@elvenspellmaker.co.uk>
2024-10-09 13:20:28 +0100

    fix: it took two runs of :format to work

--- java/src/main/java/com/ensono/stacks/core/repository/StacksPersistence.java
+++ java/src/main/java/com/ensono/stacks/core/repository/StacksPersistence.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 @SuppressWarnings(
     "java:S2326") // "T is not used in the interface". This Interface is used by other modules in
-                  // other stacks repos.
+// other stacks repos.
 public interface StacksPersistence<T> {}
